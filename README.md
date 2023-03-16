ba7798ff0806c5e7c40d3a9737d99119cd79b38c
sikmar97 <sikmar35@gmail.com>
2023-03-17 00:12:51 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# shopingcart-js
+basic shoppingcart made in javascript.
---
+++ products.js
@@ -0,0 +1,20 @@
+const products = [
+  {
+    id: 0,
+    name: "Gamecube",
+    price: 29.99,
+    instock: 100,
+    description:
+      "The GameCube is a home video game console by Nintendo in Japan from 2001.",
+    imgSrc: "./img/p1.png",
+  },
+  {
+    id: 1,
+    name: "Super Nintendo",
+    price: 24.99,
+    instock: 43,
+    description:
+      "The Super Nintendo is a home video game console by Nintendo in Japan from 1990",
+    imgSrc: "./img/p2.png",
+  },
+];
